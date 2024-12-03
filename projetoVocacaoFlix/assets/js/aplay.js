dc62ca32189f06445847b8769716b36253884b44
YASMIM ROSAL SILVA <yasmimrosalsilva2005@gmail.com>
2024-12-03 14:41:48 -0300

    Delete projetoVocacaoFlix directory

--- projetoVocacaoFlix/assets/js/aplay.js
+++
@@ -1,9 +0,0 @@
-const nav = document.getElementById('nav')
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= 90) {
-        nav.classList.add('nav__black');
-    } else {
-        nav.classList.remove('nav__black')
-    }
-})
